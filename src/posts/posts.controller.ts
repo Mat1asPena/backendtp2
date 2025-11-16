041f1aed9759c878e6809ac6bce53918aceddbee
Matias Agustin Peña <148594557+Mat1asPena@users.noreply.github.com>
2025-11-15 22:53:24 -0300

    backend sprint 2 pulido

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,40 +1,46 @@
-import { Body, Controller, Delete, Get, Param, Post, Query, Req, Request } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('posts')
 export class PostsController {
     constructor(private posts: PostsService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
-    create(@Body() body: any) {
+    create(@Body() body: any, @Req() req) {
+        body.author = req.user.username;
         return this.posts.create(body);
     }
 
-    @Get()
-    list(
-        @Query('offset') offset = '0',
-        @Query('limit') limit = '10',
-        @Query('order') order = 'fecha',
-        @Query('user') user?: string,
-    ) {
-        const off = parseInt(offset, 10) || 0;
-        const lim = parseInt(limit, 10) || 10;
-        const sort: Record<string, 1 | -1> = order === 'likes' ? { likes: -1 } : { createdAt: -1 };
-        const filter: any = { activo: true };
-        if (user) filter.author = user;
-        return this.posts.list(filter, off, lim, sort);
-    }
+    // @Get()
+    // list(
+    //     @Query('offset') offset = '0',
+    //     @Query('limit') limit = '10',
+    //     @Query('order') order = 'fecha',
+    //     @Query('user') user?: string,
+    // ) {
+    //     const off = parseInt(offset, 10) || 0;
+    //     const lim = parseInt(limit, 10) || 10;
+    //     const sort: Record<string, 1 | -1> = order === 'likes' ? { likes: -1 } : { createdAt: -1 };
+    //     const filter: any = { activo: true };
+    //     if (user) filter.author = user;
+    //     return this.posts.list(filter, off, lim, sort);
+    // }
 
+    @UseGuards(JwtAuthGuard)
     @Post(':id/like')
     like(@Param('id') id: string, @Req() req: any) {
         return this.posts.likePost(id, req.user.id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id/like')
     unlike(@Param('id') id: string, @Req() req: any) {
         return this.posts.unlikePost(id, req.user.id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     softDelete(@Param('id') id: string, @Req() req: any) {
         return this.posts.softDelete(id, req.user.id, req.user.profile);
